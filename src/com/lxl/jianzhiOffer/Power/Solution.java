c007fc00093269cd6ec682d3f96666c8b064edb0
lixiaole <franky8805@gmail.com>
2018-09-25 10:46:40 +0800

    给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方

---
+++ src/com/lxl/jianzhiOffer/Power/Solution.java
@@ -0,0 +1,28 @@
+package com.lxl.jianzhiOffer.Power;
+
+/**
+ * @author lixiaole
+ * @date 2018/9/25
+ */
+
+//给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+public class Solution{
+    public static void main(String[] args) throws Exception {
+        double a = Power(2,2);
+        System.out.println(a);
+
+        System.out.println(0>>1);
+    }
+    public static double Power(double base, int exponent) throws Exception {
+        double result = 1;
+        if(base == 0) return 0;
+        if(exponent == 0) return 1;
+        int n = exponent > 0 ? exponent : -exponent;
+        result = Power(base , n>>1);
+        result *= result;
+        if((n&1) ==1){
+            result *= base;
+        }
+        return exponent > 0 ? result : 1/result;
+    }
+}
