1fad5b3074975ccc2b2e4e74c8153787e295f46a
lixiaole <franky8805@gmail.com>
2019-09-26 15:45:12 +0800

    调整数组顺序使奇数位于偶数前面

--- src/com/needayea/jianzhiOffer/reOrderArray/Solution.java
+++ src/com/needayea/jianzhiOffer/reOrderArray/Solution.java
@@ -14,27 +14,42 @@ package com.needayea.jianzhiOffer.reOrderArray;
  *      4 将新数组的元素复制到原数组中
  */
 public class Solution {
-    public void reOrderArray(int[] array){
-        if(array==null || array.length<1) return;
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        reOrderArray(arr);
+        for(int i : arr){
+            System.out.print(i+"\t");
+        }
+    }
+
+    public static void reOrderArray(int[] array) {
+        if(array == null || array.length<1){
+            return;
+        }
+        // 统计奇数的个数
+        int count = 0;
         int start = 0;
-        int count = 0; // 用来统计奇数的个数
-        for(int i = 0 ;i<array.length;i++){
-            if((array[i]&1)==1){
+        // 奇数的二进制最低位都是1  1&1=1
+        while(start<array.length){
+            if((array[start++]&1) == 1){
                 count++;
             }
         }
 
         int[] newArray = new int[array.length];
-        for(int i = 0 ;i<array.length ;i++){
-            if((array[i]&1)==1){
+        start = 0;
+        for(int i = 0; i<array.length;i++){
+            if((array[i]&1) == 1){
                 newArray[start++] = array[i];
             }else {
                 newArray[count++] = array[i];
             }
         }
-
-        for(int i = 0 ;i<array.length;i++){
-            array[i] = newArray[i];
+        // 新数组的元素copy到原数组
+        start = 0;
+        for (int i : newArray) {
+            array[start++] = i;
         }
     }
 }
