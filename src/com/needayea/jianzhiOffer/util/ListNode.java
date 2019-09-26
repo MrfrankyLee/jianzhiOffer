ee6369e63e5a578d12db1a40232ad699c17e5052
lixiaole <franky8805@gmail.com>
2019-09-26 17:55:57 +0800

    添加工具类

---
+++ src/com/needayea/jianzhiOffer/util/ListNode.java
@@ -0,0 +1,15 @@
+package com.needayea.jianzhiOffer.util;
+
+/**
+ * @author lixiaole
+ * @date 2019-09-26
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/com/needayea/jianzhiOffer/util/TreeNode.java
@@ -0,0 +1,14 @@
+package com.needayea.jianzhiOffer.util;
+
+/**
+ * @author lixiaole
+ * @date 2019-09-26
+ */
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
