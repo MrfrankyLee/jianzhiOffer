7f445ff06595d0f2464e60e675f602807db42ffd
lixiaole <franky8805@gmail.com>
2019-09-26 11:13:02 +0800

    旋转数组中的最小数字

--- src/com/needayea/jianzhiOffer/minNumberInRotateArray/Solution.java
+++ src/com/needayea/jianzhiOffer/minNumberInRotateArray/Solution.java
@@ -1,26 +1,26 @@
 package com.needayea.jianzhiOffer.minNumberInRotateArray;
 
 /**
+ *  旋转数组中的最小数字
+ *  把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ *  输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+ *  例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+ *  NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ *
  * @author lixiaole
  * @date 2018/9/21 17:44
- */
-
-/**
- * ��һ�������ʼ�����ɸ�Ԫ�ذᵽ�����ĩβ�����ǳ�֮Ϊ�������ת�� ����һ���Ǽ�����������һ����ת�������ת�������СԪ�ء�
- * ��������{3,4,5,1,2}Ϊ{1,2,3,4,5}��һ����ת�����������СֵΪ1�� NOTE������������Ԫ�ض�����0���������СΪ0���뷵��0����
- *
- * ˼����
- *  ������Ŀ��˼����ΪĿ��������һ���Ǽ����������  ����ֻ��Ҫ�ҵ���һ���Լ�С����ǰ���Ԫ�ؼ���
- *      ��δ�ҵ�   ��˵����һ��Ԫ��Ϊ��С��Ԫ��
  */
 public class Solution {
     public static int minNumberInRotateArray(int [] array) {
         if(array==null || array.length<1){
             return 0;
         }
+        // 取第一个元素为最小元素
         int result = array[0] ;
         for(int i = 0 ; i<array.length-1;i++){
+            // 找到第一个当前位置元素大于后面位置的元素
             if(array[i]>array[i+1]){
+                // 该位置元素即为最小元素
                 result = array[i+1];
             }
         }
@@ -28,7 +28,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,4,5,1,2};
-        System.out.println(minNumberInRotateArray(arr));
+        int[] arr = {4,5,6,1,2,3};
+        int[] arr1 = {1,0,1,1,1,1};
+        System.out.println(minNumberInRotateArray(arr1));
     }
 }
