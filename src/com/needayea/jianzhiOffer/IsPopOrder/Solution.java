fc0ed79be93856cd86c784ca718f358b5f647c6d
lixiaole <franky8805@gmail.com>
2019-10-08 16:45:05 +0800

    栈的压入顺序

--- src/com/needayea/jianzhiOffer/IsPopOrder/Solution.java
+++ src/com/needayea/jianzhiOffer/IsPopOrder/Solution.java
@@ -22,16 +22,19 @@ package com.needayea.jianzhiOffer.IsPopOrder;
 import java.util.Stack;
 public class Solution {
     public boolean IsPopOrder(int [] pushA,int [] popA) {
-    if(pushA.length<=0 || popA.length<=0 || pushA.length!=popA.length)return false;
-    Stack<Integer> stack = new Stack<Integer>();
-    int index = 0;
-    for(int i = 0 ;i< pushA.length ;i++){
-        stack.push(pushA[i]);
-        while(!stack.empty()&&(stack.peek() == popA[index])){
-            stack.pop();
-            index++;
+        if(pushA == null || popA == null || popA.length != pushA.length){
+            return false;
         }
-    }
-    return stack.empty();
+
+        Stack<Integer> stack = new Stack<>();
+        int popIndex = 0;
+        for(int i = 0 ; i< pushA.length ;i++){
+            stack.push(pushA[i]);
+            while(!stack.empty() && stack.peek() == popA[popIndex]){
+                stack.pop();
+                popIndex++;
+            }
+        }
+        return stack.empty();
     }
 }
